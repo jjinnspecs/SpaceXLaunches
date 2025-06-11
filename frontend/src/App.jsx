87d84ded89d2e1f1dc66ecef62f7f4081a0eaa8e
jjinspecs <j3.cariaso@gmail.com>
2025-06-11 14:53:31 +0800

    feat: change API base URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ import LaunchCard from '../components/LaunchCard';
 import Footer from '../components/Footer';
 
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://spacexlaunchesjjinnspecs.onrender.com/api';
 const LIMIT = 10;
 
 // color mode toggle
